59a6a235ba6669f61fab10d3f713ea2e3815824a
moshiur25524 <moshiur.rahman25524@gmail.com>
2023-09-10 14:18:08 +0600

    Carts data are verified with jwt

--- src/Providers/AuthProviders.jsx
+++ src/Providers/AuthProviders.jsx
@@ -11,6 +11,7 @@ import {
   updateProfile,
 } from "firebase/auth";
 import { app } from "../firebase/firebase.config";
+import axios from "axios";
 
 export const AuthContext = createContext(null);
 
@@ -51,8 +52,20 @@ const AuthProviders = ({ children }) => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
       setUser(currentUser);
       console.log("current User", currentUser);
+
+      if (currentUser) {
+        axios
+          .post("http://localhost:5000/jwt", { email: currentUser.email })
+          .then((data) => {
+            console.log(data.data.token);
+            localStorage.setItem("access-token", data.data.token);
+          });
+      } else {
+        localStorage.removeItem("access-token");
+      }
       setLoading(false);
     });
+
     return () => {
       return unsubscribe;
     };
--- src/hooks/useCart.jsx
+++ src/hooks/useCart.jsx
@@ -4,12 +4,18 @@ import { AuthContext } from "../Providers/AuthProviders";
 
 const useCart = () => {
   const { user } = useContext(AuthContext);
+  const token = localStorage.getItem("access-token");
 
   const { refetch, data: cart = [] } = useQuery({
     queryKey: ["carts", user?.email],
     queryFn: async () => {
       const res = await fetch(
-        `http://localhost:5000/carts?email=${user?.email}`
+        `http://localhost:5000/carts?email=${user?.email}`,
+        {
+          headers: {
+            authorization: `Bearer ${token}`,
+          },
+        }
       );
       return res.json();
     },

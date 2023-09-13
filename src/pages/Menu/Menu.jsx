6f3e0196569062f7932890876ad617d86b5a235a
moshiur25524 <moshiur.rahman25524@gmail.com>
2023-09-13 19:09:39 +0600

    menuCategory images are placed to asset into scr folder

--- src/pages/Menu/Menu.jsx
+++ src/pages/Menu/Menu.jsx
@@ -1,10 +1,10 @@
 import { Helmet } from "react-helmet-async";
 import Cover from "../Shared/Cover";
-import menuImg from "../../../../assets/menu/banner3.jpg";
-import dessertImg from "../../../../assets/menu/dessert-bg.jpeg";
-import pizzaImg from "../../../../assets/menu/pizza-bg.jpg";
-import soupImg from "../../../../assets/menu/soup-bg.jpg";
-import saladImg from "../../../../assets/menu/salad-bg.jpg";
+import menuImg from "../../assets/menu/banner3.jpg";
+import dessertImg from "../../assets/menu/dessert-bg.jpeg";
+import pizzaImg from "../../assets/menu/pizza-bg.jpg";
+import soupImg from "../../assets/menu/soup-bg.jpg";
+import saladImg from "../../assets/menu/salad-bg.jpg";
 import useMenu from "../../hooks/useMenu";
 import MenuCategory from "../Shared/MenuCategory";
 
